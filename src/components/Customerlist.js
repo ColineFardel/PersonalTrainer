534880984bc79ee044e2a2afb69a597043967d49
ColineFardel <coline.fardel@gmail.com>
2020-11-25 13:24:24 +0200

    Add Edit and Delete for Customers

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -2,15 +2,36 @@ import React, { useState, useEffect, useRef } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import Snackbar from '@material-ui/core/Snackbar';
+import AddCustomer from './AddCustomer';
+import EditCustomer from './EditCustomer';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
 
 function Customerlist() {
     const [customers, setCustomers] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [msg, setMsg] = useState('');
 
     const gridRef = useRef();
 
     useEffect(() => {
         getCustomers();
     }, [])
+
+    const deleteCustomer = (link) => {
+        if (window.confirm('Are you sure ?)')) {
+            fetch(link, {
+                method: 'DELETE'
+            })
+                .then(_ => getCustomers())
+                .then(_ => {
+                    setMsg('Customer was deleted successfully');
+                    setOpen(true);
+                })
+                .catch(err => console.error(err))
+        }
+    }
 
     const columns = [
         { headerName: 'First name', field: 'firstname', sortable: true, filter: true },
@@ -20,7 +41,28 @@ function Customerlist() {
         { headerName: 'Address', field: 'streetaddress', sortable: true, filter: true },
         { headerName: 'Postcode', field: 'postcode', sortable: true, filter: true },
         { headerName: 'City', field: 'city', sortable: true, filter: true },
+        {
+            headerName: '',
+            field: '_links.self.href',
+            width: 80,
+            cellRendererFramework: params => <EditCustomer updateCustomer={updateCustomer} params={params} />
+        },
+        {
+            headerName: '',
+            field: '_links.self.href',
+            width: 80,
+            cellRendererFramework: params => <IconButton
+                color='secondary'
+                size='small'
+                onClick={() => deleteCustomer(params.value)}>
+                <DeleteIcon />
+            </IconButton>
+        },
     ]
+
+    const handleClose = () => {
+        setOpen(false);
+    }
 
     const getCustomers = () => {
         fetch('https://customerrest.herokuapp.com/api/customers')
@@ -29,10 +71,40 @@ function Customerlist() {
             .catch(err => console.error(err))
     }
 
+    const addCustomer = (newCustomer) => {
+        fetch('https://customerrest.herokuapp.com/api/customers', {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(newCustomer)
+        })
+            .then(_ => getCustomers())
+            .then(_ => {
+                setMsg('Customer added successfully');
+                setOpen(true);
+            })
+            .catch(err => console.error(err))
+    }
+
+    const updateCustomer = (link, customer) => {
+        fetch(link, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(customer)
+        })
+            .then(_ => getCustomers())
+            .then(_ => {
+                setMsg('Customer modified successfully');
+                setOpen(true);
+            })
+            .catch(err => console.error(err))
+    }
+
     return (
         <div>
             <div className="ag-theme-material" style={{ height: '700px', width: '80%', margin: 'auto' }}>
-
+                <AddCustomer addCustomer={addCustomer} />
                 <AgGridReact
                     ref={gridRef}
                     onGridReady={params => {
@@ -46,6 +118,12 @@ function Customerlist() {
                     paginationPageSize={10}
                 >
                 </AgGridReact>
+                <Snackbar
+                    open={open}
+                    autoHideDuration={3000}
+                    onClose={handleClose}
+                    message={msg}
+                />
             </div>
         </div>
     );
---
+++ src/components/EditCustomer.js
@@ -0,0 +1,131 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import EditIcon from '@material-ui/icons/Edit';
+import IconButton from '@material-ui/core/IconButton';
+
+function EditCustomer(props) {
+
+    const [open, setOpen] = useState(false);
+    const [customer, setCustomer] = useState({
+        firstname: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        address: '',
+        postcode: '',
+        city: '',
+    })
+
+    const handleClickOpen = () => {
+        console.log(props.params);
+        setCustomer({
+            firstname: props.params.data.firstname,
+            lastname: props.params.data.lastname,
+            email: props.params.data.email,
+            phone: props.params.data.phone,
+            address: props.params.data.address,
+            postcode: props.params.data.postcode,
+            city: props.params.data.city,
+        });
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const inputChanged = (event) => {
+        setCustomer({ ...customer, [event.target.name]: event.target.value });
+    };
+
+    const handleSave = () => {
+        props.updateCustomer(props.params.value, customer);
+        handleClose();
+    }
+
+    return (
+        <div>
+            <IconButton variant="outlined" color="primary" onClick={handleClickOpen}>
+                <EditIcon />
+            </IconButton>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Update Customer</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        name="firstname"
+                        value={customer.firstname}
+                        onChange={inputChanged}
+                        margin="dense"
+                        label="First name"
+                        fullWidth
+                    />
+                    <TextField
+                        name="lastname"
+                        value={customer.lastname}
+                        onChange={inputChanged}
+                        margin="dense"
+                        label="Last name"
+                        fullWidth
+                    />
+                    <TextField
+                        name="email"
+                        value={customer.email}
+                        onChange={inputChanged}
+                        margin="dense"
+                        label="Email"
+                        fullWidth
+                    />
+                    <TextField
+                        name="phone"
+                        value={customer.phone}
+                        onChange={inputChanged}
+                        margin="dense"
+                        label="Phone"
+                        fullWidth
+                    />
+                    <TextField
+                        name="address"
+                        value={customer.address}
+                        onChange={inputChanged}
+                        margin="dense"
+                        label="Address"
+                        fullWidth
+                    />
+                    <TextField
+                        name="postcode"
+                        value={customer.postcode}
+                        onChange={inputChanged}
+                        margin="dense"
+                        label="Postcode"
+                        fullWidth
+                    />
+                    <TextField
+                        name="city"
+                        value={customer.city}
+                        onChange={inputChanged}
+                        margin="dense"
+                        label="City"
+                        fullWidth
+                    />
+
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleSave} color="primary">
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    )
+}
+
+export default EditCustomer;
